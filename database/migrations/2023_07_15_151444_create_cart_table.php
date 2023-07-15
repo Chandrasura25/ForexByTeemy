411f7c2fb09726546bc3ec46c9865e800d514fd9
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-07-15 16:21:12 +0100

    cart table

--- database/migrations/2023_07_15_151444_create_cart_table.php
+++ database/migrations/2023_07_15_151444_create_cart_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('cart', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('quantity');
             $table->timestamps();
+
+            $table->foreign('product_id')
+                ->references('id')->on('products')
+                ->cascadeOnDelete();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->cascadeOnDelete();
         });
     }
 
