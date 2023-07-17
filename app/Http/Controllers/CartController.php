9db8ec17e68b6e1f03baf1793559d62f6711b752
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-07-17 15:43:14 +0100

    no repetition in cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -29,27 +29,37 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-       if(!auth()->user()){
+        if (!auth()->user()) {
             flash('You must be logged in to add items to your cart')->error();
             return redirect()->route('login');
-       }else if(auth()->user()){ 
-        $user = Auth::user();
-
-        // Get the product ID and quantity from the request
-        $product_id = $request->input('product_id');
-
-        // Create a new cart item for the user
-        $cart = new Cart();
-        $cart->user_id = $user->id;
-        $cart->product_id = $product_id;
-        $cart->quantity = 1;
-        $cart->save();
-
-        // Redirect or return a response
-        flash('Item added to cart successfully')->success();
-        return redirect()->back();
-    };
+        } else if (auth()->user()) {
+            $user = Auth::user();
+    
+            // Get the product ID and quantity from the request
+            $product_id = $request->input('product_id');
+    
+            // Check if the product already exists in the user's cart
+            $existingCartItem = Cart::where('user_id', $user->id)
+                ->where('product_id', $product_id)
+                ->first();
+    
+            if ($existingCartItem) {
+                flash('Item already exist in the cart')->info();
+            } else {
+                // If the product doesn't exist, create a new cart item for the user
+                $cart = new Cart();
+                $cart->user_id = $user->id;
+                $cart->product_id = $product_id;
+                $cart->quantity = 1;
+                $cart->save();
+                flash('Item added to cart successfully')->success();
+            }
+    
+            // Redirect back
+            return redirect()->back();
+        }
     }
+    
 
     /**
      * Display the specified resource.
