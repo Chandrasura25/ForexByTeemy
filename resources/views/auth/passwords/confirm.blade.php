c5890cbbfb24876625f4ec2410ed3b1329ab8829
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-23 18:19:17 +0100

    credential

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="body">
-    
-    <form method="POST" action="{{ route('password.confirm') }}">
-        <small class="text-light">{{ __('Please confirm your password before continuing.') }}</small>
-        <h2>{{ __('Confirm Password') }}</h2>
-         @csrf
-           <div class="inputBox">
-               <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <div class="body">
+        <form method="POST" action="{{ route('password.confirm') }}">
+            <small class="text-light">{{ __('Please confirm your password before continuing.') }}</small>
+            <h2>{{ __('Confirm Password') }}</h2>
+            @csrf
+            <div class="inputBox">
+                <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required
+                    autocomplete="current-password">
                 <label for="password">{{ __('Password') }}</label>
                 <span class="invalid-feedback" role="alert">
                     @error('password')
-                    <strong>{{ $message }}</strong>
-                </span>
+                        <strong>{{ $message }}</strong>
+                    </span>
                 @enderror
             </div>
 
             <div class="inputBox">
-                <input type="submit" value="{{ __('Confirm Password') }}">      
+                <input type="submit" value="{{ __('Confirm Password') }}">
             </div>
             <div class="row mb-0">
                 @if (Route::has('password.request'))
@@ -27,6 +27,6 @@
                     </a>
                 @endif
             </div>
-    </form>
-</div>
+        </form>
+    </div>
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,38 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-
-                <div class="body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    
-                    <form method="POST" action="{{ route('password.email') }}">
-                        <h2>{{ __('Reset Password') }}</h2>
-                        @csrf
-                        <div class="inputBox">
-                            <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            <label for="email">{{ __('Email Address') }}</label>
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="row mb-0">
-                                <button type="submit" class="btn btn-success">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                        </div>
-                    </form>
+    <div class="body">
+        <div>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-    
-<script>
-    let label = document.querySelectorAll('label').forEach(label=>{ 
-        label.innerHTML = label.innerText.split('').map((letters,i)=> `<span style="transition-delay:${i * 50}ms">${letters}</span>`).join('');
-    })
-</script>       
+            @endif
+
+            <form method="POST" action="{{ route('password.email') }}">
+                <h2>{{ __('Reset Password') }}</h2>
+                @csrf
+                <div class="inputBox">
+                    <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email"
+                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <label for="email">{{ __('Email Address') }}</label>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="row mb-0">
+                    <button type="submit" class="btn btn-success">
+                        {{ __('Send Password Reset Link') }}
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script>
+        let label = document.querySelectorAll('label').forEach(label => {
+            label.innerHTML = label.innerText.split('').map((letters, i) =>
+                `<span style="transition-delay:${i * 50}ms">${letters}</span>`).join('');
+        })
+    </script>
 @endsection
